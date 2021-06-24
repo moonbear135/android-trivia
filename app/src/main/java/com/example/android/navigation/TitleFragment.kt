08deb5ac3e26818e66e5ad40da3bd85084ce090a
Moon <redmoon11230@gmail.com>
2021-06-24 10:05:16 +0900

    전체적으로 동영상 강의가 진행이 너무 빠르고
    번역이 매끄럽지 않아 머리속에서
    따라가질 못하고 있습니다.
    
    오타발생이 많으니 주의 해야합니다.
    
    동영상에서는 코드를 작성한 후에
    그 문장을 바로 고치는 작업을 합니다.
    
    하지만 동영상이 중간중간 작성하는 과정을
    생략하는 과정이 많은 것 같습니다.
    
    작성된 코드는 주석처리하고
    수정된 코드를 한번 더 타이핑 하는 것이
    눈에 잘 보입니다.

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -25,11 +25,21 @@ class TitleFragment : Fragment() {
     ): View? {
        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
            inflater, R.layout.fragment_title,container,false)
-        //2-3,9절 4분 54초 부분
+        //2-3,9절 4분 54초부분
         //메인페이지에서 버튼을 눌러서 게임페이지로 작동하게 버튼 활성화시키기
-        binding.playButton.setOnClickListener { view: View ->
-            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
-        }
+       // binding.playButton.setOnClickListener { view: View ->
+       //     Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+       // }
+
+        //2-3,9절 5분 03초부분
+        //앞에 작성했던 코드를 Kotlin전용으로 변경 -> 좀더 관용적인 표현
+        //android KTX라고 알려져 있는 '제트 팩 프로젝트'의 일부 확장기능
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
+
+       // view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+
         return binding.root
     }
 
