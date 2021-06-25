8f0a118a8266b2466aa7fa9ffd4e44ae392d8d29
Moon <redmoon11230@gmail.com>
2021-06-26 07:18:33 +0900

    챕터2, 3장 Exercise Back Stack Manipulat 연습 백 스택 조작
    2단계까지 완료
    
    성공 / 실패 화면에서
    SUBMIT을 눌렀을 때
    
    TitleFragment 화면이 아닌
    gameFragment 화면으로
    이동하는 것을 확인 완료.
    
    네이버게이션의 뒤로가기는
    1단계에서 설정한대로
    
    TitleFragment로
    넘어가는것을 확이했습니다.

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -35,6 +35,7 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener{view: View->
             view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
         }
+
         return binding.root
     }
 }
